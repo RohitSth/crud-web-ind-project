e7eb06f8d2a5fa82e9462ae891d8624faa3997ff
RohitSth <shrestha.rohit655@gmail.com>
2023-12-18 08:51:09 +0545

    Initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+// app.js
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const sequelize = require('./database');
 const Record = require('./models/record');
@@ -7,61 +7,60 @@ const Record = require('./models/record');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); // Enable CORS for all routes
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // Create
 app.post('/create', async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const newRecord = await Record.create({ title, description });
-        res.json(newRecord);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const { title, description } = req.body;
+    const newRecord = await Record.create({ title, description });
+    res.json(newRecord);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Read
 app.get('/read', async (req, res) => {
-    try {
-        const records = await Record.findAll();
-        res.json(records);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const records = await Record.findAll();
+    res.json(records);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Update
 app.put('/update/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { title, description } = req.body;
-        await Record.update({ title, description }, { where: { id } });
-        res.json({ message: 'Record updated successfully' });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    await Record.update({ title, description }, { where: { id } });
+    res.json({ message: 'Record updated successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Delete
 app.delete('/delete/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        await Record.destroy({ where: { id } });
-        res.json({ message: 'Record deleted successfully' });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const { id } = req.params;
+    await Record.destroy({ where: { id } });
+    res.json({ message: 'Record deleted successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Sync Sequelize Models with Database and Start Server
 sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
--- database.js
+++ database.js
@@ -1,8 +1,10 @@
-// database.js
+//database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('node_crud', 'postgres', 'rstha876', {
+  host: 'localhost',
   dialect: 'postgres',
 });
 
 module.exports = sequelize;
+    
