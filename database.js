d736c9312c4d1a6d97d7886a0ad9de4f2cb4c556
RohitSth <shrestha.rohit655@gmail.com>
2023-12-19 07:21:59 +0545

    first commit

--- database.js
+++ database.js
@@ -1,10 +1,25 @@
-//database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('node_crud', 'postgres', 'rstha876', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Use the DATABASE_URL provided by Heroku
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Disable SSL verification
+      },
+    },
+  });
+} else {
+  // Use local development configuration
+  sequelize = new Sequelize('node_crud', 'postgres', 'rstha876', {
+    host: 'localhost',
+    dialect: 'postgres',
+  });
+}
 
 module.exports = sequelize;
-    
