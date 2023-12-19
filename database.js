5486c8c1948fd0b12c25bc03bbb2c55eae9eb8f2
RohitSth <shrestha.rohit655@gmail.com>
2023-12-19 07:59:24 +0545

    going back to normal

--- database.js
+++ database.js
@@ -1,37 +1,10 @@
+//database.js
 const { Sequelize } = require('sequelize');
 
-let sequelize;
-
-if (process.env.DATABASE_URL) {
-  // Use the DATABASE_URL provided by Render
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Disable SSL verification
-      },
-    },
-    logging: true, // Enable logging to see database queries in the console
-  });
-} else {
-  // Use local development configuration
-  sequelize = new Sequelize('node_crud', 'postgres', 'rstha876', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: true, // Enable logging for local development as well
-  });
-}
-
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+const sequelize = new Sequelize('node_crud', 'postgres', 'rstha876', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
 
 module.exports = sequelize;
+    
